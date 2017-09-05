b38d2befbb03a4136c0bf5660165f2a4333fef1a
psicho <9400878@gmail.com>
2017-09-05 17:53:15 +0400

    Разработка

--- 002.py
+++ 002.py
@@ -4,7 +4,21 @@
 # Решение на работает для чисел Фибоначи, оканчивающихся на 0.
 
 def fib_mod(n, m):
-    def fib_digit(n): # берём функция нахождения последней цифры n-го числа Фибоначи из предыдущей задачи 001.
+
+    def fib(n):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        s0 = 0
+        s1 = 1
+        for i in range(n - 1):
+            s = s0 + s1
+            s0 = s1
+            s1 = s
+        return s
+
+    def fib_digit(n): # берём функцию нахождения последней цифры n-го числа Фибоначи из предыдущей задачи 001.
         f1 = 0
         f2 = 1
         if n == 0:
@@ -19,10 +33,11 @@ def fib_mod(n, m):
             f1 = f2
             f2 = f
         return f
-    m = m % (len(str(m))*10)
+    m = m % 10
     print('n = ', fib_digit(n))
+    print('n0 = ', fib(n))
     print('m = ',m)
-    print('n/m = ',fib_digit(n) / m)
+    print('n/m =',fib_digit(n) / m, ' n%m =', fib_digit(n) % m, fib_digit(n) // m, ' n0%m =',fib(n) % m)
     return fib_digit(n) % m
 
-print(fib_mod(15, 33))
+print(fib_mod(10, 33))
